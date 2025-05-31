5e4bf9809b76d82b9f40de0425a4ca9d9e680074
Andrei Stepanenko <andrei@ftvkyo.me>
2025-05-31 08:46:06 +0100

    configure logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn init_logging() {
+    use env_logger::{Builder, Env};
+
+    let filter = if cfg!(debug_assertions) {
+        "info"
+    } else {
+        "warn"
+    };
+
+    let env = Env::default().default_filter_or(filter);
+
+    Builder::from_env(env).init();
+}
+
 fn main() {
-    println!("Hello, world!");
+    init_logging();
+
+    log::info!("Hello, world!");
 }
