f53364d10360e3495898b57708d183c07c087add
Andrei Stepanenko <andrei@ftvkyo.me>
2025-06-01 21:08:47 +0100

    more rendering from a tileset

--- src/game.rs
+++ src/game.rs
@@ -1,12 +1,15 @@
+use std::array::from_fn;
+
 use crate::renderer::Vertex;
 
+pub const LEVEL_SIZE: usize = 16;
+
 pub struct Tile {
-    position: [i16; 2],
     texture: [u16; 2],
 }
 
 pub struct Level {
-    tiles: Vec<Tile>,
+    tiles: [[Option<Tile>; LEVEL_SIZE]; LEVEL_SIZE],
 }
 
 impl Level {
@@ -18,39 +21,48 @@ impl Level {
             }
         };
 
+        let mut i = 0;
+
         let mut vertex_data = Vec::with_capacity(self.tiles.len() * 4);
         let mut index_data = Vec::with_capacity(self.tiles.len() * 6);
 
-        for (i, tile) in self.tiles.iter().enumerate() {
-            let i = i as u16;
+        for (x, tile_column) in self.tiles.iter().enumerate() {
+            for (y, tile) in tile_column.iter().enumerate() {
+                if let Some(tile) = tile {
+                    let x = x as i16;
+                    let y = y as i16;
 
-            vertex_data.extend_from_slice(&[
-                v(
-                    [tile.position[0], tile.position[1], 0],
-                    [tile.texture[0], tile.texture[1] + 1],
-                ),
-                v(
-                    [tile.position[0] + 1, tile.position[1], 0],
-                    [tile.texture[0] + 1, tile.texture[1] + 1],
-                ),
-                v(
-                    [tile.position[0] + 1, tile.position[1] + 1, 0],
-                    [tile.texture[0] + 1, tile.texture[1]],
-                ),
-                v(
-                    [tile.position[0], tile.position[1] + 1, 0],
-                    [tile.texture[0], tile.texture[1]],
-                ),
-            ]);
+                    vertex_data.extend_from_slice(&[
+                        v(
+                            [x, y, 0],
+                            [tile.texture[0], tile.texture[1] + 1],
+                        ),
+                        v(
+                            [x + 1, y, 0],
+                            [tile.texture[0] + 1, tile.texture[1] + 1],
+                        ),
+                        v(
+                            [x + 1, y + 1, 0],
+                            [tile.texture[0] + 1, tile.texture[1]],
+                        ),
+                        v(
+                            [x, y + 1, 0],
+                            [tile.texture[0], tile.texture[1]],
+                        ),
+                    ]);
 
-            index_data.extend_from_slice(&[
-                i * 4 + 0,
-                i * 4 + 1,
-                i * 4 + 2,
-                i * 4 + 2,
-                i * 4 + 3,
-                i * 4 + 0,
-            ]);
+                    index_data.extend_from_slice(&[
+                        i + 0,
+                        i + 1,
+                        i + 2,
+                        i + 2,
+                        i + 3,
+                        i + 0,
+                    ]);
+
+                    i += 4;
+                }
+            }
         }
 
         (vertex_data.to_vec(), index_data.to_vec())
@@ -59,20 +71,25 @@ impl Level {
 
 impl Default for Level {
     fn default() -> Self {
-        let tiles = vec![
-            Tile {
-                position: [0, 0],
-                texture: [0, 0],
-            },
-            Tile {
-                position: [1, 0],
-                texture: [0, 1],
-            },
-            Tile {
-                position: [0, 1],
-                texture: [1, 0],
-            },
-        ];
+        let mut tiles = from_fn(|_| from_fn(|_| {
+            None
+        }));
+
+        tiles[0][0] = Some(Tile {
+            texture: [0, 0],
+        });
+
+        tiles[2][0] = Some(Tile {
+            texture: [0, 3],
+        });
+
+        tiles[2][1] = Some(Tile {
+            texture: [0, 4],
+        });
+
+        tiles[3][1] = Some(Tile {
+            texture: [3, 0],
+        });
 
         Self { tiles }
     }
