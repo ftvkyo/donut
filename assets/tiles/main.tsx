eda54780fe159748c3313f8042ea91295a4c3bd2
Andrei Stepanenko <andrei@ftvkyo.me>
2025-07-04 16:16:15 +0100

    expand the default tileset

--- assets/tiles/main.tsx
+++ assets/tiles/main.tsx
@@ -1,28 +1,45 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="main" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="main-color.webp" width="64" height="64"/>
- <tile id="11" probability="0.2"/>
- <tile id="14" probability="0.3"/>
- <tile id="15" probability="0.1"/>
+<tileset version="1.10" tiledversion="1.11.2" name="main" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="main-color.webp" width="256" height="256"/>
+ <tile id="83" probability="0.2"/>
+ <tile id="84" probability="0.1"/>
+ <tile id="85" probability="0.05"/>
  <wangsets>
-  <wangset name="Main" type="corner" tile="-1">
+  <wangset name="Main" type="mixed" tile="-1">
    <wangcolor name="Wall" color="#ff0000" tile="-1" probability="1"/>
-   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
-   <wangtile tileid="1" wangid="0,0,0,0,0,1,0,0"/>
-   <wangtile tileid="2" wangid="0,1,0,0,0,1,0,1"/>
-   <wangtile tileid="3" wangid="0,1,0,1,0,0,0,1"/>
-   <wangtile tileid="4" wangid="0,1,0,0,0,0,0,0"/>
-   <wangtile tileid="5" wangid="0,0,0,0,0,0,0,1"/>
-   <wangtile tileid="6" wangid="0,0,0,1,0,1,0,1"/>
-   <wangtile tileid="7" wangid="0,1,0,1,0,1,0,0"/>
-   <wangtile tileid="8" wangid="0,0,0,0,0,1,0,1"/>
-   <wangtile tileid="9" wangid="0,1,0,0,0,0,0,1"/>
-   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="11" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="12" wangid="0,0,0,1,0,1,0,0"/>
-   <wangtile tileid="13" wangid="0,1,0,1,0,0,0,0"/>
-   <wangtile tileid="14" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="15" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="3" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="4" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="5" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="16" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="17" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="18" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="19" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="21" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="32" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="33" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="34" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="35" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="36" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="37" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="48" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="49" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="50" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="51" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="52" wangid="1,0,1,1,1,0,1,1"/>
+   <wangtile tileid="53" wangid="1,1,1,0,1,1,1,0"/>
+   <wangtile tileid="64" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="65" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="66" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="67" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="80" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="81" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="82" wangid="1,1,1,1,1,1,1,0"/>
+   <wangtile tileid="83" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="84" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="85" wangid="1,1,1,1,1,1,1,1"/>
   </wangset>
  </wangsets>
 </tileset>
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,7 +1,10 @@
-use std::time::Instant;
+use std::{
+    f32::consts::{FRAC_PI_8, PI},
+    time::Instant,
+};
 
 use anyhow::Result;
-use glam::{vec2, vec3, vec4};
+use glam::{Vec3, vec2, vec3};
 
 pub mod camera;
 pub mod light;
@@ -13,6 +16,26 @@ use crate::{
         light::{Light, Lights},
     },
 };
+
+const LIGHT_COUNT: usize = 16;
+const LIGHT_COLORS: [Vec3; LIGHT_COUNT] = [
+    vec3(1.0, 0.1, 0.1),
+    vec3(0.1, 1.0, 0.1),
+    vec3(0.1, 0.1, 1.0),
+    vec3(0.1, 1.0, 1.0),
+    vec3(1.0, 0.1, 1.0),
+    vec3(1.0, 1.0, 0.0),
+    vec3(1.0, 1.0, 0.0),
+    vec3(1.0, 0.1, 0.1),
+    vec3(0.1, 1.0, 0.1),
+    vec3(0.1, 0.1, 1.0),
+    vec3(0.1, 1.0, 1.0),
+    vec3(1.0, 0.1, 1.0),
+    vec3(1.0, 1.0, 0.0),
+    vec3(1.0, 1.0, 0.0),
+    vec3(1.0, 1.0, 1.0),
+    vec3(1.0, 1.0, 1.0),
+];
 
 pub struct Game {
     pub map_num: usize,
@@ -49,39 +72,29 @@ impl Game {
     }
 
     pub fn advance(&mut self) {
-        self.set_lights_at(self.elapsed().as_millis() % 3_000);
+        self.set_lights_at(self.elapsed().as_millis() % 4_000);
     }
 
     fn set_lights_at(&mut self, ms: u128) {
-        self.lights.inner.clear();
+        self.lights.inner = Vec::with_capacity(LIGHT_COUNT);
 
         let t = ms as f32 / 1000.0;
-        let b = 1.0 - (t / 3.0).min(0.9);
+        let b = 1.0 - (t / 5.0).min(0.9);
 
         let origin = vec3(0.0, 0.0, 0.25);
         let gravity = vec2(0.0, -3.0);
 
-        let v0s = [
-            vec2(2.0, 1.0),
-            vec2(-2.0, 1.0),
-            vec2(0.0, 4.0),
-            vec2(1.0, 3.0),
-            vec2(-1.0, 3.0),
-            vec2(3.0, 2.0),
-            vec2(-3.0, 2.0),
-        ];
+        let v0 = 7.5;
+        let angle_start = FRAC_PI_8;
+        let angle_end = PI - FRAC_PI_8;
 
-        let colors = [
-            vec4(b, 0.1, 0.1, 1.0),
-            vec4(0.1, b, 0.1, 1.0),
-            vec4(0.1, 0.1, b, 1.0),
-            vec4(0.1, b, b, 1.0),
-            vec4(b, 0.1, b, 1.0),
-            vec4(b, b, 0.0, 1.0),
-            vec4(b, b, 0.0, 1.0),
-        ];
+        for light_i in 0..LIGHT_COUNT {
+            let angle = angle_start
+                + (angle_end - angle_start) * (1.0 - light_i as f32 / (LIGHT_COUNT - 1) as f32);
+            let (angle_sin, angle_cos) = angle.sin_cos();
+            let v0 = vec2(angle_cos, angle_sin) * v0;
+            let color = LIGHT_COLORS[light_i].extend(b);
 
-        for (v0, color) in v0s.into_iter().zip(colors.into_iter()) {
             let pos = origin + v0.extend(0.0) * t + gravity.extend(0.0) * t * t;
             let vel = v0 + 2.0 * gravity * t;
 
