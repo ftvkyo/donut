af59385d214851c00a51ac8490d392eb638c5e99
Andrei Stepanenko <andrei@ftvkyo.me>
2025-07-05 12:36:46 +0100

    expand the default tileset a bit more

--- assets/tiles/main.tsx
+++ assets/tiles/main.tsx
@@ -1,45 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="main" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="main-color.webp" width="256" height="256"/>
- <tile id="83" probability="0.2"/>
- <tile id="84" probability="0.1"/>
- <tile id="85" probability="0.05"/>
+ <tile id="64" probability="0.1"/>
+ <tile id="65" probability="0.1"/>
+ <tile id="66" probability="0.05"/>
  <wangsets>
   <wangset name="Main" type="mixed" tile="-1">
-   <wangcolor name="Wall" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Wall" color="#ff0000" tile="17" probability="1"/>
    <wangtile tileid="0" wangid="0,0,1,1,1,0,0,0"/>
    <wangtile tileid="1" wangid="0,0,1,1,1,1,1,0"/>
    <wangtile tileid="2" wangid="0,0,0,0,1,1,1,0"/>
-   <wangtile tileid="3" wangid="0,0,1,0,1,0,0,0"/>
-   <wangtile tileid="4" wangid="1,0,1,0,0,0,1,0"/>
-   <wangtile tileid="5" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="4" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="5" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="6" wangid="0,0,0,0,1,0,1,0"/>
    <wangtile tileid="16" wangid="1,1,1,1,1,0,0,0"/>
    <wangtile tileid="17" wangid="1,1,1,1,1,1,1,1"/>
    <wangtile tileid="18" wangid="1,0,0,0,1,1,1,1"/>
-   <wangtile tileid="19" wangid="1,0,0,0,1,0,1,0"/>
-   <wangtile tileid="21" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="19" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="20" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="22" wangid="1,0,1,0,1,0,0,0"/>
    <wangtile tileid="32" wangid="1,1,1,0,0,0,0,0"/>
    <wangtile tileid="33" wangid="1,1,1,0,0,0,1,1"/>
    <wangtile tileid="34" wangid="1,0,0,0,0,0,1,1"/>
-   <wangtile tileid="35" wangid="1,0,1,0,0,0,0,0"/>
-   <wangtile tileid="36" wangid="0,0,1,0,1,0,1,0"/>
-   <wangtile tileid="37" wangid="1,0,0,0,0,0,1,0"/>
-   <wangtile tileid="48" wangid="0,0,0,0,1,0,0,0"/>
-   <wangtile tileid="49" wangid="0,0,1,0,0,0,0,0"/>
-   <wangtile tileid="50" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="51" wangid="0,0,0,0,0,0,1,0"/>
-   <wangtile tileid="52" wangid="1,0,1,1,1,0,1,1"/>
-   <wangtile tileid="53" wangid="1,1,1,0,1,1,1,0"/>
-   <wangtile tileid="64" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="65" wangid="1,1,1,0,1,1,1,1"/>
-   <wangtile tileid="66" wangid="1,1,1,1,1,0,1,1"/>
-   <wangtile tileid="67" wangid="1,0,1,0,1,0,1,0"/>
-   <wangtile tileid="80" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="81" wangid="1,0,1,1,1,1,1,1"/>
-   <wangtile tileid="82" wangid="1,1,1,1,1,1,1,0"/>
-   <wangtile tileid="83" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="84" wangid="1,1,1,1,1,1,1,1"/>
-   <wangtile tileid="85" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="35" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="36" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="37" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="38" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="48" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="49" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="50" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="51" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="52" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="53" wangid="1,1,1,0,1,0,1,1"/>
+   <wangtile tileid="54" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="64" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="65" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="66" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="67" wangid="1,0,1,1,1,0,1,1"/>
+   <wangtile tileid="68" wangid="1,0,1,0,1,1,1,1"/>
+   <wangtile tileid="70" wangid="1,1,1,1,1,0,1,0"/>
+   <wangtile tileid="83" wangid="1,1,1,0,1,1,1,0"/>
+   <wangtile tileid="84" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="85" wangid="1,0,1,1,1,1,1,0"/>
+   <wangtile tileid="86" wangid="1,1,1,1,1,1,1,0"/>
   </wangset>
  </wangsets>
 </tileset>
